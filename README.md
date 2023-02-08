f8edb931a286c55fdd9f217a00b7e1712fdacb68
cristino4 <117871733+cristino4@users.noreply.github.com>
2023-02-07 22:26:16 -0800

    added features: current weather/forcast data. Need to add search feature to the HTML

---
+++ README.md
@@ -0,0 +1,15 @@
+# 6-Weather-Dashboard
+Search and view the current weather and forecast for any city in the world!
+
+## Description
+Search for weather information by city. The current conditions will display along with a 5-day forecast.
+
+## Accessing the Application
+The application can be accessed on the GitHub repo here: https://github.com/cristino4/6-weather-dashboard/
+
+The application is also available on its published site: https://cristino4.github.io/6-weather-dashboard/
+
+To run the application locally, download the applications files and open the index.html file on any web browser.
+
+## Screenshot of the application
+![Screenshot of the application](./assets/images/image1.png)
---
+++ assets/js/script.js
@@ -0,0 +1,250 @@
+var apiKey = "c94957d800c39e4cc9b3130db089253a";
+var lat = "34.4856";
+var lon = "-117.3528";
+var cityName = "london";
+var limit = 10;
+var currentWeather;
+const coor = [lat,lon]
+var fiveDayForecast;
+
+currentCityEl = $("#current-city");
+currentIconEl = $("#current-icon");
+currentDescriptionEl = $("#current-description");
+currentTempEl = $("#current-temp");
+currentWindEl = $("#current-wind");
+currentHumidityEl = $("#current-humidity");
+
+$("#search-button").click(search)
+
+
+init()
+async function init(){
+   
+    //location coordinates are stored as global var
+    weatherData = await pollWeather(coor)
+    forecastData = await pollForecast(coor)
+    fiveDayForecast = parseForecast(forecastData)
+    currentWeather = parseCurrentWeather(weatherData)
+    displayForecast(currentWeather,fiveDayForecast)
+
+
+}
+
+async function search(){
+    term =$("#search-bar")[0].value;
+    
+    if (term === ""){
+        log("Error Enter a search term")
+        return
+    }
+    locations = await pollLocations(term);
+    searchRes = parseLocations(locations)
+    log(`search results: `)
+    for (i=0;i<searchRes[0].length;i++){
+        log(`#${i+1} City: ${searchRes[0][i]}, State: ${searchRes[1][i]}, Country: ${searchRes[2][i]}, Coordinates: ${searchRes[3][i]}`)
+    }
+    displayResults(searchRes)
+
+}
+
+function parseLocations(loc){
+    var cities = [];
+    var states = [];
+    var countries = [];
+    var coor = [];
+    for (let i=0;i<loc.length;i++){
+        cities.push(loc[i].name)
+        states.push(loc[i].state)
+        countries.push(loc[i].country)
+        coor.push([loc[i].lat,loc[i].lon])
+    }
+    return [cities,states,countries,coor]
+}
+
+async function pollLocations(term){
+    log(`Searching for ${term}`)
+    url = `http://api.openweathermap.org/geo/1.0/direct?q=${term}&limit=5&appid=${apiKey}`
+    const response = await fetch(url)
+    res = response.json();
+    log(`Results:`)
+    log(res)
+    return res
+
+}
+
+function displayResults(results) {
+    
+
+}
+
+// async function pollLocation(){
+
+//     return new Promise(function(resolve, reject) {
+//         navigator.geolocation.getCurrentPosition(locationFound,locationError);
+//       });
+
+
+// }
+
+// function resolve(){
+//     log("resolve")
+// }
+// function reject(){
+//     log("reject")
+// }
+function locationFound(pos){
+    const crd = pos.coords;
+    log('Current Position:');
+    log(`Latitude : ${crd.latitude}`);  
+    log(`Longitude: ${crd.longitude}`);
+    lat = crd.latitute;
+    lon = crd.longitude;
+}
+
+function locationError(){
+    log(`Location Error: ${err.code} ${err.message}`);
+    lat = null;
+    lon = null;
+}
+
+
+async function pollWeather(coor){
+    log(`Obtaining weather data for lattitude: ${coor[0]} longitude: ${coor[1]}`)
+    weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${coor[0]}&lon=${coor[1]}&appid=${apiKey}`
+    const response = await fetch(weatherUrl)
+    res = response.json()
+    log("Data:")
+    log(res)
+    return res
+
+}
+
+async function pollForecast(coor){
+    log(`Obtaining forecast data for lattitude: ${coor[0]} longitude: ${coor[1]}`)
+    weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${coor[0]}&lon=${coor[1]}&appid=${apiKey}`
+    const response = await fetch(weatherUrl)
+    res = response.json()
+    log("Data:")
+    log(res)
+    return res
+}
+
+function parseForecast(data){
+    //day = [date,temp,wind,humidity,icon,description]
+    var forecast;
+    day1 = [data.list[0].dt_txt,data.list[0].main.temp,data.list[0].wind.speed,data.list[0].main.humidity,data.list[0].weather[0].icon,data.list[0].weather[0].description]
+    day2 = [data.list[1].dt_txt,data.list[1].main.temp,data.list[1].wind.speed,data.list[1].main.humidity,data.list[1].weather[0].icon,data.list[1].weather[0].description] 
+    day3 = [data.list[2].dt_txt,data.list[2].main.temp,data.list[2].wind.speed,data.list[2].main.humidity,data.list[2].weather[0].icon,data.list[2].weather[0].description] 
+    day4 = [data.list[3].dt_txt,data.list[3].main.temp,data.list[3].wind.speed,data.list[3].main.humidity,data.list[3].weather[0].icon,data.list[3].weather[0].description] 
+    day5 = [data.list[4].dt_txt,data.list[4].main.temp,data.list[4].wind.speed,data.list[4].main.humidity,data.list[4].weather[0].icon,data.list[4].weather[0].description]  
+    forecast = [day1,day2,day3,day4,day5]
+    log(`Parsing 5 day forcast:`)
+    log(`Day 1: ${day1} `)
+    log(`Day 2: ${day2} `)
+    log(`Day 3: ${day3} `)
+    log(`Day 4: ${day4} `)
+    log(`Day 5: ${day5} `)
+    
+    return forecast
+
+}
+
+function parseCurrentWeather(data){
+    var country = data.sys.country;
+    var city = data.name;
+    var humidity = data.main.humidity;
+    var temp = data.main.temp;
+    var win = data.wind.speed;
+    var iconID = data.weather[0].icon;
+    var description = data.weather[0].description
+    log(`Parsing Weather: City ${city}, Country ${country}, Humidity ${humidity}, Temperature ${temp}, Wind ${win}, iconID ${iconID}`)
+    return [country,city,humidity,temp,win,iconID,description]
+}
+
+function displayForecast(current,forecast){
+    //display current weather
+    currentCityEl.text(`${current[1]}, ${current[0]}`)
+    currentIconEl.attr("style", `background-image: url(http://openweathermap.org/img/wn/${current[5]}.png)`);
+    currentDescriptionEl.text(current[6]);
+    currentHumidityEl.text(`Humidity: ${current[2]}`);
+    currentTempEl.text(`Temperature: ${current[3]}`);
+    currentWindEl.text(`Wind: ${current[4]}`);
+
+    //display forecast
+    //icon update
+    var icons = [];
+    for (i=0;i<5;i++){
+        icons.push($(`#d${i+1}-header`));
+    }
+    for (i=0; i<5;i++){
+        icons[i].attr("style", `background-image: url(http://openweathermap.org/img/wn/${forecast[i][4]}.png)`);
+    }
+    //description update
+    var desc = []
+    for (i=0;i<5;i++){
+        desc.push($(`#d${i+1}-desc`));
+    }
+    for (i=0; i<5;i++){
+        desc[i].text(forecast[i][5]);
+    }
+    //temp update
+    var desc = []
+    for (i=0;i<5;i++){
+        desc.push($(`#d${i+1}-temp`));
+    }
+    for (i=0; i<5;i++){
+        desc[i].text(forecast[i][1]);
+    }
+    //wind update
+    var desc = []
+    for (i=0;i<5;i++){
+        desc.push($(`#d${i+1}-wind`));
+    }
+    for (i=0; i<5;i++){
+        desc[i].text(forecast[i][2]);
+    }
+    //humidity update
+    var desc = []
+    for (i=0;i<5;i++){
+        desc.push($(`#d${i+1}-humid`));
+    }
+    for (i=0; i<5;i++){
+        desc[i].text(forecast[i][3]);
+    }
+    //dateupdate
+    var desc = []
+    for (i=0;i<5;i++){
+        desc.push($(`#d${i+1}-date`));
+    }
+    for (i=0; i<5;i++){
+        desc[i].text(forecast[i][0]);
+    }
+    
+}
+
+
+// // locationUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=${limit}&appid=${apiKey}`
+// // weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`
+
+// // fetch(weatherUrl)
+// // .then(function(response){
+// //     return response.json()
+// // })
+// // .then(function (data){
+// //     console.log(data)
+// // })
+
+
+
+// // fetch(locationUrl)
+// // .then(function(response){
+// //     return response.json()
+// // })
+// // .then(function (data){
+// //     console.log(data)
+// // })
+
+
+function log(mesg){
+    console.log(mesg)
+}
